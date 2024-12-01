76eb3c148d4862c1fcc65af073a7723fae15b0b6
ebt35 <eyoub.beraki@hotmail.com>
2024-12-01 19:39:30 +0100

    working with strings

---
+++ live_demos/09_strings/sql/clean_glossary.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA IF NOT EXISTS refined;
+
+CREATE TABLE IF NOT EXISTS refined.sql_glossary AS (
+SELECT
+	UPPER(TRIM(sql_word)) AS sql_word,
+	REGEXP_REPLACE(TRIM(description),
+	'  +',
+	' ',
+	'g' ) AS cleaned_description,
+	example
+FROM
+	staging.sql_glossary);
+
+
+SELECT * FROM refined.sql_glossary;
+
+
