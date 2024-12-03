d5b7fef5361129b72c0b6d0fc4052b946099296b
ebt35 <eyoub.beraki@hotmail.com>
2024-12-03 21:34:09 +0100

    set operations

---
+++ live_demos/10_set_operations/sql/load_sakila.sql
@@ -0,0 +1,5 @@
+INSTALL sqlite;
+LOAD sqlite;
+
+-- absolite path to sqlite-sakila.db
+CALL sqlite_attach('/Users/admin/Documents/github/sql-eyoub-beraki-de24/live_demos/10_set_operations/data/sqlite-sakila.db')
---
+++ live_demos/10_set_operations/sql/set_operations.sql
@@ -0,0 +1,34 @@
+SELECT * FROM synthetic.sales_jan
+UNION
+SELECT * FROM synthetic.sales_feb;
+
+-- note only one monitor
+SELECT product_name, amount FROM synthetic.sales_jan
+UNION
+SELECT product_name, amount FROM synthetic.sales_feb;
+
+
+-- note two monitors
+SELECT product_name, amount FROM synthetic.sales_jan
+UNION ALL
+SELECT product_name, amount FROM synthetic.sales_feb;
+
+
+-- note only row of monitor and 300 exist in both result sets
+SELECT product_name, amount FROM synthetic.sales_jan
+INTERSECT
+SELECT product_name, amount FROM synthetic.sales_feb;
+
+SELECT product_name, amount FROM synthetic.sales_jan
+EXCEPT
+SELECT product_name, amount FROM synthetic.sales_feb;
+
+-- emoty set because of different dates in monitor
+SELECT * FROM synthetic.sales_jan
+INTERSECT
+SELECT * FROM synthetic.sales_feb;
+
+
+
+
+
